ad3941145cea21a3220cddc709b41b11b7b6927d
Nikhilpy6 <100987844+Nikhilpy6@users.noreply.github.com>
2022-09-23 15:52:26 +0530

    Time: 3 ms (85.68%), Space: 49.6 MB (67.37%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,17 +1,55 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-    HashMap < Integer, Integer > mp = new HashMap < > ();
-    ArrayList < Integer > ans = new ArrayList < > ();
-    int n= nums.length;
-    for (int i = 0; i < n; i++) {
-      mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
-    }
+//     HashMap < Integer, Integer > mp = new HashMap < > ();
+//     ArrayList < Integer > ans = new ArrayList < > ();
+//     int n= nums.length;
+//     for (int i = 0; i < n; i++) {
+//       mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+//     }
 
-    for (int x: mp.keySet()) {
-      if (mp.get(x) > (n / 3))
-        ans.add(x);
-    }
+//     for (int x: mp.keySet()) {
+//       if (mp.get(x) > (n / 3))
+//         ans.add(x);
+//     }
 
+//     return ans;
+      int count1=0;
+      int count2=0;
+      int number1=-1;
+      int number2=-1;
+      int len=nums.length;
+      
+     for (int i = 0; i < len; i++) {
+      if (nums[i] == number1)
+        count1++;
+      else if (nums[i] == number2)
+        count2++;
+      else if (count1 == 0) {
+        number1 = nums[i];
+        count1 = 1;
+      } 
+      else if (count2 == 0) {
+        number2 = nums[i];
+        count2 = 1;
+      }
+      else {
+        count1--;
+        count2--;
+      }
+    }
+    ArrayList < Integer > ans = new ArrayList < Integer > ();
+    count1 = 0;
+    count2 = 0;
+    for (int i = 0; i < len; i++) {
+      if (nums[i] == number1)
+        count1++;
+      else if (nums[i] == number2)
+        count2++;
+    }
+    if (count1 > len / 3)
+      ans.add(number1);
+    if (count2 > len / 3)
+      ans.add(number2);
     return ans;
-    }
+  }
 }
