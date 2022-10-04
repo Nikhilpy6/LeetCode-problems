cf65f5d42ade7c9a90899e44c8df031c90b9e6e8
Nikhilpy6 <100987844+Nikhilpy6@users.noreply.github.com>
2022-10-04 11:47:09 +0530

    Time: 211 ms (46.90%), Space: 129.3 MB (77.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -3,6 +3,7 @@ class Solution {
         //  This is used to sort the 2D array on the basis of fist element or second  ............
         Arrays.sort(properties,(b,a)->a[0]==b[0] ? b[1]-a[1] : a[0]-b[0]);
       
+      
         int max=Integer.MIN_VALUE;
         int c=0;
         for(int i=0;i<properties.length;i++){
