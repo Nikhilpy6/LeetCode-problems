8e894a5c396d97c5bd72f11598f95e16b8814ff4
Nikhilpy6 <100987844+Nikhilpy6@users.noreply.github.com>
2022-10-04 11:28:10 +0530

    Time: 120 ms (88.77%), Space: 88.6 MB (97.59%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfWeakCharacters(int[][] properties) {
+        //  This is used to sort the 2D array on the basis of fist element or second  ............
+        Arrays.sort(properties,(b,a)->a[0]==b[0] ? b[1]-a[1] : a[0]-b[0]);
+      
+        int max=Integer.MIN_VALUE;
+        int c=0;
+        for(int i=0;i<properties.length;i++){
+        //  System.out.println(properties[i][1]+" ");
+            if(properties[i][1]<max)
+                c++;
+            else
+                max=Math.max(max,properties[i][1]);
+        }
+        return c;
+    }
+}
