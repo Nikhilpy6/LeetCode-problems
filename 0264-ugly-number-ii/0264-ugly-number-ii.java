ef4b21b7791fbada2b9e6ebb67f0152a1e38d05b
Nikhilpy6 <100987844+Nikhilpy6@users.noreply.github.com>
2022-10-18 01:19:30 +0530

    Time: 4 ms (84.76%), Space: 41.9 MB (71.23%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int nthUglyNumber(int n) {
         int dp[]=new int[n];
       dp[0]=1;
-      int p1=0;
+      int p1=0;  //pointers
       int p2=0;
       int p3=0;
       for(int i=1;i<n;i+=1){
@@ -10,7 +10,12 @@ class Solution {
         int mul3=dp[p2]*3; //3*1=3
         int mul5=dp[p3]*5; //5*1=5
          
-        dp[i]=Math.min(mul2,Math.min(mul3,mul5));
+        dp[i]=Math.min(mul2,Math.min(mul3,mul5));  
+        
+        //dp[]= [1,2,3,4,5,6,8......dp[n-1]]
+        //p1+=1
+        //min(2*4,3*3,5*1)==>3
+        //now p2+=1
         
         if(dp[i]==mul2)p1++;
         if(dp[i]==mul3)p2++;
