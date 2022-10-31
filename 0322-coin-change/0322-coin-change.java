c2bd6d229f3fd8b691ef8a109ebf1614d94bc7ba
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2022-11-01 01:19:21 +0530

    Time: 34 ms (52.40%), Space: 54.8 MB (18.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int func(int[] coins,int amount,int ind,int [][]dp){
       if(ind==0){
         if(amount%coins[0]==0)
@@ -23,4 +23,34 @@ class Solution {
      if(ans >= (int)1e9)return -1;
       return ans;
     }
+}*/
+
+
+
+
+class Solution {
+   
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        int dp[][]=new int[coins.length][amount+1];
+       for(int T=0;T<=amount;T++){
+         if(T%coins[0]==0)dp[0][T]=T/coins[0];
+         else dp[0][T]=(int)1e9;
+       }
+      
+      for(int ind=1;ind<n;ind++){
+        for(int T =0;T<=amount;T++){
+          int nottake= dp[ind-1][T];
+        int take=Integer.MAX_VALUE;
+        if(coins[ind]<=T){
+          take =1 + dp[ind][T-coins[ind]];
+        }
+          dp[ind][T]=Math.min(take,nottake);
+        }
+      }
+     int ans=dp[n-1][amount];
+     if(ans >= (int)1e9)return -1;
+      return ans;
+    }
 }
+
