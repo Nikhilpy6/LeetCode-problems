81cafdb26852e26c268cb005634cdf9c4efe8810
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2022-10-31 06:07:01 +0530

    Time: 2 ms (42.33%), Space: 45.7 MB (62.59%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+          for(int j=0;j<matrix[0].length;j++){
+            if(i<matrix.length-1 && j<matrix[0].length-1 && matrix[i][j]!=matrix[i+1][j+1])
+              return false;
+          }
+        }
+      return true;
+    }
+}
