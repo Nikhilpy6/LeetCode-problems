d4e4aac04d336bc67107ac662dab8a4cb0f286ba
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-05-18 00:56:36 +0530

    Time: 21 ms (75.17%), Space: 43.1 MB (55.24%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
@@ -0,0 +1,57 @@
+class Solution {
+  class Pair{
+    int pos;
+    boolean isBackward;
+    
+    Pair(int pos,boolean isBackward){
+      this.pos=pos;
+      this.isBackward=isBackward;
+    }
+  }
+    public int minimumJumps(int[] forbidden, int a, int b, int x) {
+           // start + a
+         //start -b
+       // min(a,b) and start>=0 and forbidden doesnot contains start
+      
+      //set to check whether forward move or backward move pos in set or not in  O(1) time
+      Set<Integer>visited=new HashSet<>();
+        for(int e:forbidden){
+            visited.add(e);
+        }
+
+        Queue<Pair>q=new LinkedList<>();
+        q.add(new Pair(0,false));
+        visited.add(0);
+
+        int limit=6000;// given in constarints (2000+2000+2000)
+        int level=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            while(size-->0){
+                int pos=q.peek().pos;
+                boolean isBackward=q.peek().isBackward;
+                q.poll();
+
+                if(pos==x) return level;
+
+                if(!isBackward){//if false take backward move
+
+                    int nextBackPos=pos-b;
+                    if(nextBackPos>0 && !visited.contains(nextBackPos)){
+                        visited.add(nextBackPos);
+                        q.add(new Pair(nextBackPos,true));
+                    }
+                    
+                }
+                int nextForPos=pos+a;
+                if(nextForPos<limit && !visited.contains(nextForPos)){
+                    visited.add(nextForPos);
+                    q.add(new Pair(nextForPos,false));
+                }   
+            }
+            level++;
+        }
+        return -1;
+      
+    }
+}
