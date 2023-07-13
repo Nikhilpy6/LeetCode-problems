c1642db1d0195d69ff051ad0251b7f9af352b434
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-07-14 03:35:24 +0530

    Time: 11 ms (63.59%), Space: 43.3 MB (40.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,6 @@
 class Solution {
-  
+   int dp[][];
+  int total;
     public int solve(int idx,int[] nums,int target,int sum){
       
       if(idx==nums.length){
@@ -7,13 +8,19 @@ class Solution {
         else 
           return 0;
       }
-      
+      if(dp[idx][sum+total]!=Integer.MIN_VALUE)return dp[idx][sum+total];
       int ad=solve(idx+1,nums,target,sum+nums[idx]);
       int su=solve(idx+1,nums,target,sum-nums[idx]);
       
-      return ad+su;
+      return dp[idx][sum+total]=ad+su;
     }
     public int findTargetSumWays(int[] nums, int target) {
+     total=Arrays.stream(nums).sum();
+      
+      dp=new int[nums.length][2*total+1];
+      for(int i=0;i<nums.length;i++){
+        Arrays.fill(dp[i],Integer.MIN_VALUE);
+      }
        return solve(0,nums,target,0);
              
     }
