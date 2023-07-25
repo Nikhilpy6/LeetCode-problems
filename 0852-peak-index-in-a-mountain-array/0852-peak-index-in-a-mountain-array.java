73aace2d899c1f5e7b1ef4933f78b2510fa8c8d7
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-07-26 00:49:44 +0530

    Time: 1 ms (15.54%), Space: 55.3 MB (67.26%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,21 +1,40 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
       
-     int i=0;
-      int j=arr.length-1;
+//      int i=0;
+//       int j=arr.length-1;
+//       int n=arr.length;
+//       while(i<j){
+//         int mid = (i + j) / 2;
+//             if ((mid == 0 || arr[mid - 1] < arr[mid]) && (mid == n - 1 || arr[mid + 1] < arr[mid]))
+//                 return mid;
+//             else if (mid > 0 && arr[mid - 1] > arr[mid])
+//                 j = mid - 1;
+//             else
+//                 i = mid + 1;
+        
+        
+//       }
+//       return i;
+     
+ 
+      
+      
+      
+//Solution 2
+      
       int n=arr.length;
-      while(i<j){
-        int mid = (i + j) / 2;
-            if ((mid == 0 || arr[mid - 1] < arr[mid]) && (mid == n - 1 || arr[mid + 1] < arr[mid]))
-                return mid;
-            else if (mid > 0 && arr[mid - 1] > arr[mid])
-                j = mid - 1;
-            else
-                i = mid + 1;
+       int i=0;
+     for(i=0;i<n-1;i++){
+        if(arr[i+1]<arr[i])
+          break;
         
         
+      
       }
       return i;
+      
+      
     }
     
 }
