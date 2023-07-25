34bce391fd263d1e90bd7f342c117f3da276186c
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-07-26 00:56:01 +0530

    Time: 36 ms (38.60%), Space: 44.4 MB (63.89%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,22 +1,22 @@
 class MyCalendar {
-    List<int[]> calendar;
+//     List<int[]> calendar;
 
-    MyCalendar() {
-        calendar = new ArrayList();
-    }
+//     MyCalendar() {
+//         calendar = new ArrayList();
+//     }
 
-    public boolean book(int start, int end) {
-        for (int[] iv: calendar) {
-            if (iv[0] < end && start < iv[1]) return false;
-        }
-        calendar.add(new int[]{start, end});
-        return true;
-    }
-}
+//     public boolean book(int start, int end) {
+//         for (int[] iv: calendar) {
+//             if (iv[0] < end && start < iv[1]) return false;
+//         }
+//         calendar.add(new int[]{start, end});
+//         return true;
+//     }
+// }
   
   
   
-  /*
+  
     TreeMap<Integer,Integer> tm;
     public MyCalendar() {
       tm = new TreeMap<>();
@@ -32,27 +32,17 @@ class MyCalendar {
 //       int lk=tm.lowKey(start)==null ? ;
 //       int uk=tm.ceilingKey(end);
       
+     
+      int lk=tm.floorKey(start)==null ? -1 : tm.floorKey(start);
+      int uk=tm.ceilingKey(start)==null ? -1 : tm.ceilingKey(start);
       
-      if(tm.isEmpty()){
+      if((lk==-1 || tm.get(lk)<=start)&& (uk==-1 || end<=uk)){
         tm.put(start,end);
+        return true;
       }
-      int lk=tm.floorKey(start)!=null ? -1 : tm.floorKey(start);
-      int uk=tm.ceilingKey(end);
-      
+      return false;
     }
-  
-//   class pair{
-//     int f;
-//     int s;
-    
-//     pair(int f,int s){
-//       this.f=f;
-//       this.s=s;
-//     }
-//   }
-  
-  
-  
+
 }
 
 /**
