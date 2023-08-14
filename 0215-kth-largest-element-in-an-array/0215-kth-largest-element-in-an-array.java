58ae0ae0533df8abaafdc882359f4ee340128fc3
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-08-15 01:56:42 +0530

    Time: 28 ms (64.91%), Space: 54.2 MB (85.75%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,23 +1,56 @@
 class Solution {
+  
+   void swap(int l,int j,int []nums){
+     
+     int t=nums[l];
+     nums[l]=nums[j];
+     nums[j]=t;
+   }
+  
+  
+  
+    public int partitition(int l,int r,int []nums){
+      int i=l+1;
+      int j=r;
+      int pivot=nums[l];
+      
+      while(i<=j){
+        if(nums[i]<pivot && nums[j]>pivot){
+          swap(i,j,nums);
+          i++;
+          j--;
+        }
+        if(nums[i]>=pivot)
+          i++;
+        
+        if(nums[j]<=pivot)
+          j--;
+     }
+      
+      swap(l,j,nums);
+      return j;
+    }
+  
+  
+  
+  
     public int findKthLargest(int[] nums, int k) {
-        //Arrays.sort(nums);
-        //return nums[nums.length-k];
-      
-      
-     //priorityQueue in java store the values in minheap by default
-    PriorityQueue<Integer> pq =new PriorityQueue<>();
-      for(int i=0;i<nums.length;i++){
-        pq.add(nums[i]);
+        //quick sort 
+      int l=0;
+      int r=nums.length-1;
+      int pivot_idx=0;
+      while(true){
+        pivot_idx=partitition(l,r,nums);
         
-      } 
-      
-      
-      while(k<pq.size()){
-         
-        pq.poll();
+        if(pivot_idx==k-1)break;
         
-       
+        if(pivot_idx<k-1){//2<3
+          l=pivot_idx+1;
+        }else{
+          r=pivot_idx-1;
+        
       }
-     return pq.peek(); 
+    }
+        return nums[pivot_idx];
     }
 }
