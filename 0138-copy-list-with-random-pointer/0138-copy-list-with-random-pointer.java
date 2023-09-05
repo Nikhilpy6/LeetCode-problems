05fa31236766f914c1f6eb53c0014f98743a0115
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-09-05 23:27:24 +0530

    Time: 0 ms (100.00%), Space: 43 MB (75.75%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,33 +15,7 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-//         Node cur = head;
-//       while(cur!=null){  // inserting new nodes
-//         Node temp = cur.next;
-//         cur.next=new Node(cur.val);
-//         cur.next.next=temp;
-//         cur=temp;
-//       }
-//       cur=head;
-//       while(cur!=null){
-//         if(cur.next!=null){
-//           cur.next.random=(cur.random!=null)?cur.random : null;
-//         }
-//         cur=cur.next.next;
-//       }
-      
-//       Node orig=head,copy=head.next;
-//       Node temp=copy;
-//       while(orig!=null){
-//         orig.next=orig.next.next;
-//         copy.next=copy.next.next;
-//         orig=orig.next;
-//         copy=copy.next;
-//       }
-//       return temp;
-      
-      
-      
+    
       
       if (head == null)
       return null;
