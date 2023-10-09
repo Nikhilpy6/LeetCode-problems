8bc1f39c35620e75a426a0ecf1d77e57ccefe1ca
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2023-10-10 01:12:18 +0530

    Time: 46 ms (5.04%), Space: 55.3 MB (5.00%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,34 +1,63 @@
 class Solution {
-    int m, n;
-    int[][] t;
-
+  // memoization
+  int t[][];
+  public int solve(int i,int j,int []nums1,int []nums2){
+    if(i==nums1.length || j==nums2.length)
+        return (int)(-10000009);
+      
+    if(t[i][j]!=-10000009)return t[i][j];  
+      int single_ele = nums1[i] * nums2[j];
+      
+      int takeij = solve(i+1,j+1,nums1,nums2) + single_ele;
+      
+      int takei = solve(i+1,j,nums1,nums2) ;
+      
+      int takej = solve(i,j+1,nums1,nums2) ;
+      
+      return t[i][j] = Math.max(single_ele,Math.max(takeij,Math.max(takei,takej)));
+    
+    
+  }
+  
+  
+  
+  
+  /*
+  recursive approach
+  
+  
+    public int solve(int i,int j,int []nums1,int []nums2){
+      
+      if(i==nums1.length || j==nums2.length)
+        return (int)(-10000009);
+      
+      
+      int single_ele = nums1[i] * nums2[j];
+      
+      int takeij = solve(i+1,j+1,nums1,nums2) + single_ele;
+      
+      int takei = solve(i+1,j,nums1,nums2) ;
+      
+      int takej = solve(i,j+1,nums1,nums2) ;
+      
+      return Math.max(single_ele,Math.max(takeij,Math.max(takei,takej)));
+      
+      
+    }*/
+  
+  
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        m = nums1.length;
-        n = nums2.length;
-        t = new int[501][501];
-
-        for (int i = 0; i < 501; i++) {
-            Arrays.fill(t[i], -100000000);
-        }
-
-        return solve(nums1, nums2, 0, 0);
-    }
-
-    public int solve(int[] nums1, int[] nums2, int i, int j) {
-        if (i == m || j == n)
-            return -100000000;
-
-        if (t[i][j] != -100000000)
-            return t[i][j];
-
-        int val = nums1[i] * nums2[j];
-
-        int take_i_j = solve(nums1, nums2, i + 1, j + 1) + val;
-        int take_i = solve(nums1, nums2, i, j + 1);
-        int take_j = solve(nums1, nums2, i + 1, j);
-
-        t[i][j] = Math.max(val, Math.max(take_i_j, Math.max(take_i, take_j)));
-        return t[i][j];
+        // such case  that are
+      //   --> i frm nums1 and j frm nums2 (single element)
+      //  --> i+1 frm  nums1 and j+1 frm nums2(for rest other)
+      // --> i+1 frm nums1 and j frm nums2
+      // --> i frm nums1 and j+1 frm nums2
+      // out of the se four conditions maximum of these will be our desire output
+      t = new int[1001][1001];
+      for(int i=0;i<1001;i++){
+        Arrays.fill(t[i],(int)(-10000009));
+      }
+      return solve(0,0,nums1,nums2);
       
     }
 }
