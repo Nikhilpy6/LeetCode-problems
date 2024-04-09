d784e780afa7bcf2aa3e558038aaa176d7f1ce40
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2024-04-10 02:44:11 +0530

    Time: 0 ms (100.00%), Space: 41 MB (78.23%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,36 +1,37 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         
-      //Approach 1(Using queue)
+  /*    Approach 1(Using queue)
       
       
-//       int c=0;
+      int c=0;
       
-//       Queue<Integer> qu = new LinkedList<>();
+      Queue<Integer> qu = new LinkedList<>();
        
-//       for(int i=0;i<tickets.length;i++){
-//         qu.add(i);
-//       }
+      for(int i=0;i<tickets.length;i++){
+        qu.add(i);
+      }
       
-//       while(!qu.isEmpty()){
-//         int front = qu.poll();
-//         c++;
-//         tickets[front]--;
+      while(!qu.isEmpty()){
+        int front = qu.poll();
+        c++;
+        tickets[front]--;
         
-//         if(front==k && tickets[front]==0){
-//           return c;
-//         }
+        if(front==k && tickets[front]==0){
+          return c;
+        }
         
-//         if(tickets[front]!=0){
+        if(tickets[front]!=0){
           
-//           qu.add(front);
+          qu.add(front);
           
-//         }
+        }
         
-//       }
-//       return c;
+      }
+      return c; */
       
-      //Approach 2 without usng QUeue
+      
+    /*  Approach 2 without usng QUeue
       
       int c=0;
       
@@ -54,6 +55,30 @@ class Solution {
         
       }
       return c;
+    */  
+      
+      
+      // Approach -3
+      
+      int time=0;
+      
+      for(int i=0;i<tickets.length;i++){
+        if(i<=k){
+          time+=Math.min(tickets[i],tickets[k]);
+        }
+        
+        else{
+          if(tickets[i]>=tickets[k]){
+            time+=tickets[k]-1;
+          }
+          else{
+            time+=tickets[i];
+          }
+        }
+      }
+      
+      return time;
+      
       
     }
 }
