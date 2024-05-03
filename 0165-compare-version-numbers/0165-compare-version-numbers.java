adcd3185c54a326a0ebb67ddc12fdb838877bb93
NikHil GuPta <100987844+Nikhilpy6@users.noreply.github.com>
2024-05-03 22:32:31 +0530

    Time: 1 ms (78.87%), Space: 41.8 MB (5.03%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+      List<String> v1 = getTokens(version1);
+      List<String> v2 = getTokens(version2);
+      
+      int i=0;
+      int m = v1.size();
+      int n = v2.size();
+      while(i<m || i<n){
+        int a = i<m ? Integer.parseInt(v1.get(i)) : 0;
+        int b = i<n ? Integer.parseInt(v2.get(i)) : 0;
+        if(a>b)
+          return 1;
+        else if (a<b)
+          return -1;
+        else
+          i++;
+        
+      }
+      return 0;
+    }
+  
+   private List<String> getTokens(String version) {
+        String[] tokens = version.split("\\.");
+        List<String> versionTokens = new ArrayList<>();
+        for (String token : tokens) {
+            versionTokens.add(token);
+        }
+        return versionTokens;
+    }
+}
